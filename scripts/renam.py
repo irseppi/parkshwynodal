32e4aed7651185dd9c653fd57812bd9d75f44440
Isabella Seppi <irseppi@alaska.edu>
2023-10-27 14:38:26 -0800

    remove

--- scripts/renam.py
+++
@@ -1,8 +0,0 @@
-import os
-os.getcwd()
-collection = '533595343'
-for i, filename in enumerate(os.listdir(collection)):
-	for p, fil in enumerate(os.listdir('533595343/'+filename)):
-		
-		os.rename('533595343/' + filename + '/' + fil, '533595343/' + filename +'_'+ fil)
-
