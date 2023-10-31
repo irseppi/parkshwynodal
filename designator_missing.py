96fabd769ab6e7d5cdf0ee9358851d77c7acd28d
Isabella Seppi <irseppi@alaska.edu>
2023-10-31 10:17:56 -0800

    update

--- designator_missing.py
+++ designator_missing.py
@@ -65,4 +65,12 @@ for i in range(len(flight_names)):
 			f.write(val[10]+','+val[0]+','+val[1]+','+val[2]+','+val[3]+','+val[4]+','+val[5]+','+val[6]+','+val[7]+','+val[8]+'\n')
 
 f.close()				
-
+#for equip with no designator
+text = open('output/designator_missing.txt','r')
+f = open('output/equp_missing.txt','w')
+for line in text.readlines():
+	val = line.split(',')
+	print(val[4])
+	if val[4] == '':
+		f.write(val[0]+','+val[1]+','+val[2]+','+val[3]+','+val[4]+','+val[5]+','+val[6]+','+val[7]+','+val[8]+','+val[9])
+f.close()
