1dd54eba4ae50ee3cea673761c1196e262787c33
Isabella Seppi <irseppi@alaska.edu>
2024-01-12 14:01:20 -0900

    remove absoloute value on spectrum

--- spec_fft.py
+++ spec_fft.py
@@ -49,7 +49,6 @@ for line in text.readlines():
 				fs = int(tr[0].stats.sampling_rate)
 				title    = f'{tr[0].stats.network}.{tr[0].stats.station}.{tr[0].stats.location}.{tr[0].stats.channel} − starting {tr[0].stats["starttime"]}'						
 				t                  = tr[0].times()
-				#data               = tr[0].data()
 			except:
 				continue
 			
@@ -85,11 +84,83 @@ for line in text.readlines():
 				fs = int(tr[2].stats.sampling_rate)
 				title    = f'{tr[2].stats.network}.{tr[2].stats.station}.{tr[2].stats.location}.{tr[2].stats.channel} − starting {tr[2].stats["starttime"]}'						
 				t                  = tr[2].times()
-				#data               = tr[2].data()
 			except:
 				continue
 	
 		try:
+
+			# Time array
+			t = np.arange(len(data)) / fs
+			g = fs*240
+
+			# Compute spectrogram
+			frequencies, times, Sxx = spectrogram(data, fs, scaling='density', nperseg=fs, noverlap=fs * .9) 
+
+			fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(8,6))     
+
+			ax1.plot(t, data, 'k', linewidth=0.5)
+			ax1.set_title(title)
+			ax1.axvline(x=tim, c = 'r', ls = '--')
+			ax1.margins(x=0)
+			vmin = np.min(10 * np.log10(Sxx))
+			vmax = np.max(10 * np.log10(Sxx))
+
+			# Plot spectrogram
+			cax = ax2.pcolormesh(times, frequencies, 10 * np.log10(Sxx), shading='gouraud', cmap='hsv', vmin=vmin, vmax=vmax)
+			ax2.set_xlabel('Time [s]')
+
+			# Find the center of the trace
+			center_index = len(data) // 2
+			center_time = t[center_index]
+
+			ax2.axvline(x=center_time, c = 'r', ls = '--')
+			ax2.set_ylabel('Frequency (Hz)')
+			ax2.margins(x=0)
+			ax3 = fig.add_axes([0.9, 0.11, 0.015, 0.35])
+
+			plt.colorbar(mappable=cax, cax=ax3)
+
+			ax3.set_ylabel('Relative Amplitude (dB)')
+			BASE_DIR = "/scratch/irseppi/nodal_data/plane_info/plane_spec/2019-0"+str(month1)+"-"+str(day1)+"/"+flight_num+ '/'+station
+			make_base_dir(BASE_DIR)
+			fig.savefig('/scratch/irseppi/nodal_data/plane_info/plane_spec/2019-0'+str(month1)+'-'+str(day1)+'/'+flight_num + '/'+station+'/'+str(time)+'_'+flight_num+'.png')
+			plt.close()
+
+			# Compute and plot amplitude spectrum for the center of the trace
+			# Find the index of the middle frequency
+			middle_index = len(times) // 2
+
+			# Extract the middle line of the spectrogram
+			middle_column = Sxx[:, middle_index]
+
+			# Plot the middle line of the spectrogram
+			plt.figure(figsize=(10,6))
+			plt.grid()
+			plt.plot(frequencies, 10 * np.log10(middle_column))
+			plt.title('Amplitude Spectrum at t = {:.2f} s'.format(center_time))
+
+			plt.xlim(2,int(fs/2))
+
+			plt.xlabel('Freq [Hz]')
+			plt.ylabel('Amplitude [dB]')
+			plt.title('Amplitude Spectrum at t = {:.2f} s'.format(center_time))
+			plt.close()
+
+			BASE_DIR = '/scratch/irseppi/nodal_data/plane_info/spec/'+str(val[0])+'/'+str(val[1])+'/'+str(val[5])+'/'
+			make_base_dir(BASE_DIR)
+			plt.savefig('/scratch/irseppi/nodal_data/plane_info/spec/'+str(val[0])+'/'+str(val[1])+'/'+str(val[5])+'/'+str(val[5])+'_' + str(val[2]) + '.png')
+			plt.close()
+
+		except:
+			continue	
+
+
+
+
+
+
+
+			'''
 			# Time array
 			t = np.arange(len(data)) / fs
 			g = fs*240
@@ -148,7 +219,7 @@ for line in text.readlines():
 			make_base_dir(BASE_DIR)
 			plt.savefig('/scratch/irseppi/nodal_data/plane_info/spec2/'+str(val[0])+'/'+str(val[1])+'/'+str(val[5])+'/'+str(val[5])+'_' + str(val[2]) + '.png')
 			plt.close()
-			'''
+
 			plt.figure()
 			# Compute spectrogram
 			frequencies, times, Sxx = spectrogram(data, fs, scaling='density', nperseg=fs, noverlap=fs * .9)
@@ -168,5 +239,4 @@ for line in text.readlines():
 			plt.show()
 			plt.close()
 			'''
-		except:
-			continue
+
