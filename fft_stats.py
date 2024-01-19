22e2a050021c3955a583df85bd3b122cdefb1bf2
Isabella Seppi <irseppi@alaska.edu>
2024-01-18 15:12:40 -0900

    stats

---
+++ fft_stats.py
@@ -0,0 +1,127 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import spectrogram, windows
+import obspy
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io
+import os
+import obspy
+from obspy.core import UTCDateTime
+import datetime
+from numpy.fft import fft, ifft
+from pathlib import Path
+from scipy import signal
+
+def make_base_dir(base_dir):
+	base_dir = Path(base_dir)
+	if not base_dir.exists():
+		current_path = Path("/")
+		for parent in base_dir.parts:
+			current_path = current_path/parent
+			if not current_path.exists():
+				current_path.mkdir()
+
+text = open('input/all_station_crossing_db.txt', 'r')
+#output = open('okay.txt','w')
+
+for line in text.readlines():
+	val = line.split(',')
+	time = val[2]
+	ht = datetime.datetime.utcfromtimestamp(int(val[2]))
+	mins = ht.minute
+	secs = ht.second
+	h = ht.hour
+	month1 = ht.month
+	day1 = ht.day
+	station = str(val[5])
+	plane = str(val[6])
+	tim = 120
+	
+	if ht.month == 2 and ht.day >= 11 or ht.month == 3:
+		day_of_year = str((ht - datetime.datetime(2019, 1, 1)).days + 1)
+		
+		if val[5].isdigit() == False:
+			try:
+				n = "/aec/wf/2019/0"+day_of_year+"/"+str(val[5])+".*Z.20190"+day_of_year+"000000+"
+				tr = obspy.read(n)
+				
+				tr[0].trim(tr[0].stats.starttime +(h *60 *60) + (mins * 60) + secs - tim, tr[0].stats.starttime +(h *60 *60) + (mins * 60) + secs + tim)
+				data = tr[0][0:-1]
+				fs = int(tr[0].stats.sampling_rate)
+				title    = f'{tr[0].stats.network}.{tr[0].stats.station}.{tr[0].stats.location}.{tr[0].stats.channel} − starting {tr[0].stats["starttime"]}'						
+				t                  = tr[0].times()
+			except:
+				continue
+			
+		else:	
+			month2 = str(month1)
+			if day1 < 10:
+				day1 = '0'+str(day1)	
+			if h < 23:			
+				day2 = str(day1)
+				if h < 10:
+					h_u = '0'+str(h+1)
+					h = '0'+str(h)
+				else:
+					h_u = str(h+1)
+					h = str(h)
+				
+			else:
+				h_u = '00'
+				if month1 == '2' and day1 == '28':
+					month2 = '03'
+					day2 = '01'
+				else:	
+					if int(day1) >= 10:
+						day2 = str(int(day1) + 1)
+					else:
+						day2 = '0'+str(int(day1)+1)
+			try:		
+				n = "/scratch/naalexeev/NODAL/2019-0"+str(month1)+"-"+str(day1)+"T"+str(h)+":00:00.000000Z.2019-0"+month2+"-"+day2+"T"+h_u+":00:00.000000Z."+station+".mseed"
+				tr = obspy.read(n)
+			
+				tr[2].trim(tr[2].stats.starttime + (mins * 60) + secs - tim, tr[2].stats.starttime + (mins * 60) + secs + tim)
+				data = tr[2][0:-1]
+				fs = int(tr[2].stats.sampling_rate)
+				title    = f'{tr[2].stats.network}.{tr[2].stats.station}.{tr[2].stats.location}.{tr[2].stats.channel} − starting {tr[2].stats["starttime"]}'						
+				t                  = tr[2].times()
+			except:
+				continue
+		base_dir = Path('input/plane_overtones/a'+plane[0:-1]+'.txt')
+		
+		try:
+			# Time array
+			t = np.arange(len(data)) / fs
+			g = fs*240
+
+			# Compute spectrogram
+			frequencies, times, Sxx = spectrogram(data, fs, scaling='density', nperseg=fs, noverlap=fs * .9) 
+
+			
+			# Find the index of the middle frequency
+			middle_index = len(times) // 2
+
+			# Extract the middle line of the spectrogram
+			middle_column = Sxx[:, middle_index]
+			peaks, _ = signal.find_peaks(10 * np.log10(middle_column), prominence=10, distance = 10) 
+			np.diff(peaks)
+			#info = np.array(peaks, properties)
+			print('len'+str(len(peaks)))
+			if os.path.exists(base_dir):
+				output = open(base_dir,'a')
+			else:
+				output = open(base_dir,'w')
+			 #+'\n')
+			#output.write(info)
+			#for g in range(len(peaks)):
+			#print(peaks[g])
+				#output.write(peaks[g]+'\n')
+			#print(str(info[0][g])) #+'\n') #, 10 * np.log10(middle_column[peaks[l]]))
+			#output.write('try\n') #peaks[l], 10 * np.log10(middle_column[peaks[l]])) #station+'\n'+'peak_freq:'+peaks+'\n'+'amp'+10 * np.log10(middle_column[peaks])+'\n')
+			output.close()
+				
+		except:
+			continue
+
