7446e391706a3cb96c754d1841d375b6ae07b216
Isabella Seppi <irseppi@alaska.edu>
2024-03-04 11:36:37 -0900

    'test'

--- old_spec.py
+++ old_spec.py
@@ -11,10 +11,7 @@ from obspy.geodetics import gps2dist_azimuth
 from prelude import make_base_dir, distance
 import datetime
 from obspy import UTCDateTime
-
-
-
-
+from prelude import make_base_dir, distance, closest_encounter
 seismo_data = pd.read_csv('input/all_sta.txt', sep="|")
 seismo_latitudes = seismo_data['Latitude']
 seismo_longitudes = seismo_data['Longitude']
