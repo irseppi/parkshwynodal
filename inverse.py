ae078ebdee5289b748e7f8fe86ca03996687c321
Isabella Seppi <irseppi@alaska.edu>
2024-03-06 17:08:46 -0900

    'update environment'

--- inverse.py
+++ inverse.py
@@ -10,24 +10,35 @@ from obspy.core import UTCDateTime
 import datetime
 from prelude import make_base_dir, distance, closest_encounter
 
-def df(f0,v0,l,t):
+def df(f0,v0,l,tprime0):
+
     c = 343 # m/sec speed of sound
-    tprime = np.sqrt(l**2-(v0*t)**2/c)
-    t = (tprime - np.sqrt(tprime**2-(1-v0**2/c**2)(tprime**2-l**2/c**2)))/(1-v0**2/c**2)
-
-    f = f0*1/(1+(v0/c)*(v0*t/(np.sqrt(l**2-(v0*t)**2))))
-
-
+    tprime = np.sqrt(l**2-(v0*tprime0)**2/c)
+   
+    t = (tprime - np.sqrt(tprime**2-(1-v0**2/c**2)*(tprime**2-l**2/c**2)))/(1-v0**2/c**2)
+    f = f0*1/(1+(v0/c)*(v0*t/(np.sqrt(l**2+(v0*t)**2))))
+   
+    ft0p = f0*1/(1+(v0/c)*(v0*((tprime - tprime0)- np.sqrt((tprime-tprime0)**2-(1-v0**2/c**2)*((tprime-tprime0)**2-l**2/c**2)))/(1-v0**2/c**2)/(np.sqrt(l**2+(v0*((tprime - tprime0)- np.sqrt((tprime-tprime0)**2-(1-v0**2/c**2)*((tprime-tprime0)**2-l**2/c**2)))/(1-v0**2/c**2))**2))))
     #derivative with respect to f0
-    f_derivef0 = 1/(1+(v0/c)*(v0*t/(np.sqrt(l**2-(v0*t)**2))))
+    #f_derivef0 = 1/(1+(v0/c)*(v0*t/(np.sqrt(l**2+(v0*t)**2))))
+    f_derivef0 = np.gradient(ft0p, f0)
+
     #derivative of f with respect to v0
-    f_derivev0 = c*f0*t*v0*(t**2*v0**2-l**2)*1/(np.sqrt(l**2-(v0*t)**2)*(c*np.sqrt(l**2-(v0*t)**2)+t*v0**2)**2)
+    f_derivev0 = np.gradient(ft0p, v0) 
+    print(f_derivev0)
+
     #derivative of f with respect to l
-    f_derivel = (c*f0*t*v0**2*l)*1/(np.sqrt(l**2-(v0*t)**2)*(c*np.sqrt(l**2-(v0*t)**2)+t*v0**2)**2)
+    f_derivel = np.gradient(ft0p, l)
+
     #derivative of f with respect to tprime
-    f_derivetprime = (c**3*f0*l**2*v0**2*(c*np.sqrt((-(v0**2*tprime**2)*c**2)+tprime**2-(l**2/c**2)))+(v0**2-c**2)*tprime)/(np.sqrt((-(v0**2*tprime**2)*c**2)+tprime**2-(l**2/c**2))*np.sqrt(l**2-v0*(-np.sqrt(((-(v0**2*tprime**2)/c**2)+tprime**2-(l**2/c**2)))+tprime-(v0**2/c**2))**2)*(c**3*np.sqrt(l**2-v0*(-np.sqrt(((-(v0**2*tprime**2)/c**2)+tprime**2-(l**2/c**2)))+tprime-(v0**2/c**2)))-c**2*v0**2*np.sqrt((-(v0**2*tprime**2)/c**2)+tprime**2-(l**2/c**2))+c**2*v0**2*tprime-v0**4)**2)
-    
-    return f_derivef0, f_derivev0, f_derivel, f_derivetprime, tprime
+    #f_derivetprime0 = -(c**3 * f0 * l**2 * v0**2 * (c * np.sqrt((-(v0**2 * tprime**2) * c**2) + tprime**2 - (l**2 / c**2))) + (v0**2 - c**2) * tprime) / (np.sqrt((-(v0**2 * tprime**2) * c**2) + tprime**2 - (l**2 / c**2)) * np.sqrt(l**2 - v0 * (-np.sqrt(((-(v0**2 * tprime**2) / c**2) + tprime**2 - (l**2 / c**2))) + tprime - (v0**2 / c**2)))**2 * (c**3 * np.sqrt(l**2 - v0 * (-np.sqrt(((-(v0**2 * tprime**2) / c**2) + tprime**2 - (l**2 / c**2))) + tprime - (v0**2 / c**2))) - c**2 * v0**2 * np.sqrt((-(v0**2 * tprime**2) / c**2) + tprime**2 - (l**2 / c**2)) + c**2 * v0**2 * tprime - v0**4)**2)
+    f_derivetprime0 = np.gradient(ft0p, tprime0)
+
+    return f_derivef0, f_derivev0, f_derivel, f_derivetprime0, tprime
+
+
+
+
 
 
 def invert_f(m0, coords_array, num_iterations):
@@ -39,16 +50,17 @@ def invert_f(m0, coords_array, num_iterations):
     m = m0
     for t in range(n):
         tpa = []
+        fnew = []
+        #for i in range(len(coords_array)):
+        f_derivef0, f_derivev0, f_derivel, f_derivetprime, tprime = df(m[0], m[1], m[2], m[3])
+       
+        #partial derivative matrix of f with respect to m when m=m0
         for i in range(len(coords_array)):
-            f_derivef0, f_derivev0, f_derivel, f_derivetprime, tprime = df(m[0], m[1], m[2], m[3])
-            tpa.append(tprime)
-            #partial derivative matrix of f with respect to m when m=m0
-            G[i,:] = [[f_derivef0, f_derivev0, f_derivel, f_derivetprime]] 
-
-            #update m
-            f = m[0]*1/(1+(m[1]/c)*(m[1]*m[3]/(np.sqrt(m[2]**2-(m[1]*m[3])**2))))
-            m = m0 +inv(G.T@G)@G.T[fobs - f]
-            m0 = m
+            G[i,0:4] = [f_derivef0[i], f_derivev0[i], f_derivel[i], f_derivetprime[i]]
+            fnew.append(m[0]*1/(1+(m[1]/c)*(m[1]*int(m[3][i])/(np.sqrt(m[2]**2+(m[1]*m[3][i]))**2)))) # Convert m[3][i] to integer
+     
+            m = np.array(np.array([m0[0],m[1],m[2],m[3][i]])) +inv(G.T@G)@G.T@[fobs - np.array(fnew)] #update m fix this
+        m0 = m
     return m
 
 
@@ -93,73 +105,74 @@ for n in range(0,5):
         if str(tm[line]) == str(time[n]):
             speed = flight_data['speed'][line]
             alt = flight_data['altitude'][line]
-        for y in range(len(station)):
-            if str(station[y]) == str(sta[n]):
-                dist = distance(seismo_latitudes[y], seismo_longitudes[y], flight_latitudes[line], flight_longitudes[line])	
+            for y in range(len(station)):
+                if str(station[y]) == str(sta[n]):
+                    dist = distance(seismo_latitudes[y], seismo_longitudes[y], flight_latitudes[line], flight_longitudes[line])	
 
-                p = "/scratch/naalexeev/NODAL/2019-02-"+str(day[n])+"T"+str(h)+":00:00.000000Z.2019-02-"+day2+"T"+h_u+":00:00.000000Z."+station[y]+".mseed"
-                tr = obspy.read(p)
-                tr[2].trim(tr[2].stats.starttime + (mins * 60) + secs - tim, tr[2].stats.starttime + (mins * 60) + secs + tim)
-                data = tr[2][0:-1]
-                fs = int(tr[2].stats.sampling_rate)
-                title = f'{tr[2].stats.network}.{tr[2].stats.station}.{tr[2].stats.location}.{tr[2].stats.channel} − starting {tr[2].stats["starttime"]}'						
-                t = tr[2].times()
-                # Time array
-                t = np.arange(len(data)) / fs
-                g = fs*240
-                # Compute spectrogram
-                frequencies, times, Sxx = spectrogram(data, fs, scaling='density', nperseg=fs, noverlap=fs * .9, detrend = 'constant') 
+                    p = "/scratch/naalexeev/NODAL/2019-02-"+str(day[n])+"T"+str(h)+":00:00.000000Z.2019-02-"+day2+"T"+h_u+":00:00.000000Z."+station[y]+".mseed"
+                    tr = obspy.read(p)
+                    tr[2].trim(tr[2].stats.starttime + (mins * 60) + secs - tim, tr[2].stats.starttime + (mins * 60) + secs + tim)
+                    data = tr[2][0:-1]
+                    fs = int(tr[2].stats.sampling_rate)
+                    title = f'{tr[2].stats.network}.{tr[2].stats.station}.{tr[2].stats.location}.{tr[2].stats.channel} − starting {tr[2].stats["starttime"]}'						
+                    t = tr[2].times()
+                    # Time array
+                    t = np.arange(len(data)) / fs
+                    g = fs*240
+                    # Compute spectrogram
+                    frequencies, times, Sxx = spectrogram(data, fs, scaling='density', nperseg=fs, noverlap=fs * .9, detrend = 'constant') 
 
-                a, b = Sxx.shape
+                    a, b = Sxx.shape
 
-                MDF = np.zeros((a,b))
-                for row in range(len(Sxx)):
-                    m = len(Sxx[row])
-                    p = sorted(Sxx[row])
-                    median = p[int(m/2)]
-                    for col in range(m):
-                        MDF[row][col] = median
-                spec = 10 * np.log10(Sxx) - (10 * np.log10(MDF))		
-                middle_index = len(times) // 2
-                middle_column = spec[:, middle_index]
-                vmin = 0  
-                vmax = np.max(middle_column) 
+                    MDF = np.zeros((a,b))
+                    for row in range(len(Sxx)):
+                        m = len(Sxx[row])
+                        p = sorted(Sxx[row])
+                        median = p[int(m/2)]
+                        for col in range(m):
+                            MDF[row][col] = median
+                    spec = 10 * np.log10(Sxx) - (10 * np.log10(MDF))		
+                    middle_index = len(times) // 2
+                    middle_column = spec[:, middle_index]
+                    vmin = 0  
+                    vmax = np.max(middle_column) 
 
-                coords = []
-                plt.figure()
-                plt.pcolormesh(times, frequencies, spec, shading='gouraud', cmap='pink_r', vmin=vmin, vmax=vmax)
-                
+                    coords = []
+                    plt.figure()
+                    plt.pcolormesh(times, frequencies, spec, shading='gouraud', cmap='pink_r', vmin=vmin, vmax=vmax)
+                    
 
-                
-                def onclick(event):
-                    global coords
-                    coords.append((event.xdata, event.ydata))
-                    plt.scatter(event.xdata, event.ydata, color='black', marker='x')  # Add this line
-                    plt.draw() 
-                    print('Clicked:', event.xdata, event.ydata)  
+                    
+                    def onclick(event):
+                        global coords
+                        coords.append((event.xdata, event.ydata))
+                        plt.scatter(event.xdata, event.ydata, color='black', marker='x')  # Add this line
+                        plt.draw() 
+                        print('Clicked:', event.xdata, event.ydata)  
 
-                cid = plt.gcf().canvas.mpl_connect('button_press_event', onclick)
+                    cid = plt.gcf().canvas.mpl_connect('button_press_event', onclick)
 
-                plt.show(block=True)
-                # Convert the list of coordinates to a numpy array
-                coords_array = np.array(coords)
+                    plt.show(block=True)
+                    # Convert the list of coordinates to a numpy array
+                    coords_array = np.array(coords)
 
 
-                f0 = 120
-                t = coords_array[:,0]
-                c = 343
-                v0 = speed*0.514444
-                l = closest_encounter(flight_latitudes, flight_longitudes,line, tm, seismo_latitudes[y], seismo_longitudes[y])
-                m0 = [f0, v0, l, t]
-                m = invert_f(m0, coords_array, 8)
+                    f0 = 120
+                    t = coords_array[:,0]
+                    c = 343
+                    v0 = speed*0.514444
+                    l = closest_encounter(flight_latitudes, flight_longitudes,line, tm, seismo_latitudes[y], seismo_longitudes[y])
+                    m0 = [f0, v0, l, t]
 
-                f = m[0]*1/(1+(m[1]/c)*(m[1]*m[3]/(np.sqrt(m[2]**2-(m[1]*m[3])**2))))
+                    m = invert_f(m0, coords_array, 8)
 
-                plt.figure()
-                plt.pcolormesh(times, frequencies, spec, shading='gouraud', cmap='pink_r', vmin=vmin, vmax=vmax)
-                plt.plot(f, m[3])
-                plt.show()
+                    f = m[0]*1/(1+(m[1]/c)*(m[1]*m[3]/(np.sqrt(m[2]**2+(m[1]*m[3])**2))))
 
-                make_base_dir('/scratch/irseppi/nodal_data/plane_info/5inv_spec/')
-                plt.save('/scratch/irseppi/nodal_data/plane_info/5inv_spec/2019-02-'+str(day[n])+'/'+str(flight_num[n])+'/'+station[y]+'.png')
-                plt.close()
+                    plt.figure()
+                    plt.pcolormesh(times, frequencies, spec, shading='gouraud', cmap='pink_r', vmin=vmin, vmax=vmax)
+                    plt.plot(f, m[3])
+                    plt.show()
+
+                    make_base_dir('/scratch/irseppi/nodal_data/plane_info/5inv_spec/')
+                    plt.save('/scratch/irseppi/nodal_data/plane_info/5inv_spec/2019-02-'+str(day[n])+'/'+str(flight_num[n])+'/'+station[y]+'.png')
+                    plt.close()
