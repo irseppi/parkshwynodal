Trying to fix map projection and calculation of colsest vertical distance between flight path and seismic station

cmap_auto.py
@@ -40,7 +40,7 @@ for line in sta_f.readlines():
                 if sta[t] == station:
 
                     clat, clon, dist, ctime = closest_encounter(flight_latitudes, flight_longitudes, l, time, seismo_latitudes[t], seismo_longitudes[t])
-                    f = 1.0/np.cos(clat*np.pi/180)
+                    f = 1.0/np.cos((clat+(seismo_latitudes[t]-clat)/2)*np.pi/180)
                     ht = datetime.datetime.utcfromtimestamp(time[l])
 
                     # Create a figure with two subplots side by side
@@ -70,6 +70,7 @@ for line in sta_f.readlines():
                     maxl = xx + 0.05
                     minla = yy - 0.03
                     maxla = yy + 0.03
+
                     heading = np.deg2rad(head[l])
                     
                     rect = Rectangle((minl, minla), 0.1, 0.06, ls="-", lw = 1, ec = 'k', fc="none", zorder=2.5)
@@ -80,11 +81,9 @@ for line in sta_f.readlines():
                     axs[1].plot(flight_longitudes, flight_latitudes, c='#377eb8',linestyle ='dotted')
                     axs[1].set_xlim(minl, maxl)
                     axs[1].set_ylim(minla, maxla)
-                    #axs[1].text(seismo_longitudes[t], seismo_latitudes[t], sta[t], fontsize=11, fontweight='bold')
                     axs[1].tick_params(axis='both', which='major', labelsize=10)
                     
-                    
-                    #axs[1].text(flight_longitudes[l], flight_latitudes[l], ht, fontsize=11, fontweight='bold')
+                     
                     m = (flight_latitudes[l+1] - flight_latitudes[l])/(flight_longitudes[l+1] - flight_longitudes[l])
                     b = flight_latitudes[l] - m*flight_longitudes[l]
 

inverse.py
@@ -12,6 +12,7 @@ auto_peak_pick = False
 seismo_data = pd.read_csv('input/all_sta.txt', sep="|")
 seismo_latitudes = seismo_data['Latitude']
 seismo_longitudes = seismo_data['Longitude']
+elevations = seismo_data['Elevation']
 station = seismo_data['Station']
 flight_num = [530342801,528485724,528473220,528407493,528293430,527937367,529741194,529776675,529179112,530165646,531605202,531715679,529805251,529948401] 
 time = [1551066051,1550172833,1550168070,1550165577,1550089044,1549912188,1550773710,1550787637,1550511447,1550974151,1551662362,1551736354,1550803701,1550867033] 
@@ -277,7 +278,7 @@ for n in range(0,13):
                         f0 = fs/4
                         tprime0 = tarrive
                         v0 = speed_mps
-                        l = np.sqrt(dist_m**2 + alt_m**2)
+                        l = np.sqrt(dist_m**2 + (alt_m-elevations[y])**2)
 
                     c = 343
                     m0 = [f0, v0, l, tprime0]