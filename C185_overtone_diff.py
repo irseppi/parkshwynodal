9e7962a3796a40b7186a69770cc970a17fcbed87
Isabella Seppi <irseppi@alaska.edu>
2024-10-11 12:43:21 -0800

    Median difference between overtones

---
+++ C185_overtone_diff.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Define the directory where your files are located
+file = open('output/C185data_updated.txt', 'r')
+f1 = []
+peaks_filt = []
+
+# Read the data from the file and append it to the respective lists based on the mode
+for line in file.readlines():
+    lines = line.split(',')
+    quality_num = int(lines[9])
+
+    peaks = lines[7]
+
+    peaks = np.char.replace(peaks, '[', '')
+    peaks = np.char.replace(peaks, ']', '')
+
+    peaks = str(peaks)
+    peaks = np.array(peaks.split(' '))
+
+    for peak in peaks:
+        if peak == '' or peak == ' ' or peak == '   ':
+            continue
+        else:
+            peaks_filt.append(peak)
+
+    for p in range(len(peaks_filt)):
+        if p == 0:
+            continue
+        else:
+            diff = float(peaks_filt[p]) - float(peaks_filt[p-1])
+            f1.append(diff)
+
+
+plt.figure()
+plt.hist(f1, bins=500)
+plt.axvline(x=np.median(f1), color='red', linestyle='--')
+
+plt.text(20, 78000, 'Median: '+str(np.round(np.median(f1),2)))
+
+plt.xlim(0,68)
+plt.show()
