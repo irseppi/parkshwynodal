c8c3a13b474dee811f2b734a375cc1d18780b39e
Isabella Seppi <irseppi@alaska.edu>
2024-10-17 16:10:27 -0800

    Correction

--- C185_overtone_diff.py
+++ C185_overtone_diff.py
@@ -3,8 +3,6 @@ import numpy as np
 
 file = open('output/C185data_updated.txt', 'r')
 f1 = []
-peaks_filt = []
-
 for line in file.readlines():
     lines = line.split(',')
     quality_num = int(lines[9])
@@ -16,26 +14,43 @@ for line in file.readlines():
 
     peaks = str(peaks)
     peaks = np.array(peaks.split(' '))
-
+    peaks_filt = []
     for peak in peaks:
         if peak == '' or peak == ' ' or peak == '   ':
             continue
         else:
-            peaks_filt.append(peak)
-
+            peaks_filt.append(float(peak))
+    peaks_filt = sorted(peaks_filt)
+    print(peaks_filt)
     for p in range(len(peaks_filt)):
+
         if p == 0:
             continue
         else:
             diff = float(peaks_filt[p]) - float(peaks_filt[p-1])
+            print(float(peaks_filt[p]), float(peaks_filt[p-1]))
+            print(diff)
+            '''
+            if 10 > diff:
+                continue
+            elif diff > 30:
+                continue
+                #missing = diff / 20
+                #for m in range(int(missing)):
+                #    f1.append()
+            
+            else:
+            '''
             f1.append(diff)
 
 
 plt.figure()
 plt.hist(f1, bins=500)
 plt.axvline(x=np.median(f1), color='red', linestyle='--')
+plt.axvline(x=np.mean(f1), color='red', linestyle='--')
 
-plt.text(20, 78000, 'Median: '+str(np.round(np.median(f1),2)))
+plt.text(20, 30, 'Median: '+str(np.round(np.median(f1),2)))
+plt.text(20, 20, 'Mean: '+str(np.round(np.mean(f1),2)))
 
-plt.xlim(0,68)
+#plt.xlim(0,140)
 plt.show()
