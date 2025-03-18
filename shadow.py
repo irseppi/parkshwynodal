cee929337d77e9868c411305ca6c7e943d4cc29c
Isabella Seppi <irseppi@alaska.edu>
2025-03-18 10:17:56 -0800

    Add flag

--- shadow.py
+++ shadow.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+option = 'Temp'
+#option = 'Type'
+if option == 'Temp':
+    file = open('1o_atmc_v_2c.txt','r')
+elif option == 'Type':
+    file = open('2c_1o_v_full.txt','r')
 
-#file = open('1o_atmc_v_2c.txt','r')
-#file = open('temp_correction_1ovfull.txt','r')
-file = open('2c_1o_v_full.txt','r')
 file2 = pd.read_csv('/home/irseppi/REPOSITORIES/parkshwynodal/input/all_station_crossing_db_C185.csv', sep=",")
 tail_nums = file2['TAIL_NUM']
 flight = file2['FLIGHT_NUM']
@@ -33,7 +36,7 @@ med_new = []
 for line in file.readlines():
     y += 1
     counts = []
-
+#ax1.set_title('Diffrence in Frequency outcome between Temperature Correction vs Constant -2 degrees C (One Overtone inversion)'
     lines = line.split(',')
     time_old.append(float(lines[3]))
     time_new.append(float(lines[8]))
@@ -86,23 +89,26 @@ for line in file.readlines():
     med_new.append(np.nanmedian(f2))
     date_all.append(y)
 
-fig,ax1 = plt.subplots(1, 1, sharex=False, figsize=(8,6))     
+fig,ax1 = plt.subplots(1, 1, sharex=False, figsize=(8,6))    
 
 ax1.margins(x=0)
 ax2 = fig.add_axes([0.83, 0.11, 0.07, 0.77], sharey=ax1)
 
-#ax1.scatter(pppp_old, date_old, c='b', label='-2C')
-#ax1.scatter(pppp_new, date_new,c='r',label='Temperature Corrected')
-ax1.scatter(pppp_old, date_old, c='b', label='One Overtone')
-ax1.scatter(pppp_new, date_new, c='r',label='Full')
+if option == 'Temp':
+    ax1.scatter(pppp_old, date_old, c='b', label='-2C')
+    ax1.scatter(pppp_new, date_new,c='r',label='Temperature Corrected')
+    ax1.set_title('Diffrence in Frequency outcome between Temperature Correction vs Constant -2 degrees C (One Overtone inversion)')
 
+elif option == 'Type':
+    ax1.scatter(pppp_old, date_old, c='b', label='One Overtone')
+    ax1.scatter(pppp_new, date_new, c='r',label='Full')
+    ax1.set_title('Diffrence in Frequency between One Overtone Inversion and Full Inversion (-2C)')
 ax2.scatter(med_old, date_all, c='b')
 ax2.scatter(med_new, date_all, c='r')
 
 ax1.tick_params(left=False, right=False, labelleft=False, labelbottom=True, bottom=True)
 ax2.tick_params(left=False, right=False, labelleft=False, labelbottom=True, bottom=True)
-#ax1.set_title('Diffrence in Frequency outcome between Temperature Correction vs Constant -2 degrees C (One Overtone inversion)')
-ax1.set_title('Diffrence in Frequency between One Overtone Inversion and Full Inversion (-2C)')
+
 ax1.set_xlabel('Frequency')
 ax2.set_xlabel('\u0394'+'F')
 ax1.legend(loc='upper left',fontsize = 'x-small')
@@ -110,4 +116,4 @@ ax1.set_xlim(0, 300)
 ax1.set_xticks(range(0, 251, 25)) 
 plt.legend()
 plt.show()
-#
+
