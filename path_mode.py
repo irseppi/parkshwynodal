ade612eb6bd3d3a814749ca916e8b3df3eaf71e0
Isabella Seppi <irseppi@alaska.edu>
2025-04-04 13:54:13 -0800

    And arrows to plot. Use the full inversion data

--- path_mode.py
+++ path_mode.py
@@ -4,7 +4,6 @@ import pandas as pd
 import pyproj
 import pygmt
 from prelude import *
-from scipy.interpolate import interp1d
 
 #Load seismometer data
 seismo_data = pd.read_csv('/home/irseppi/REPOSITORIES/parkshwynodal/input/all_sta.txt', sep="|")
@@ -23,7 +22,7 @@ seismo_utm_x, seismo_utm_y = zip(*seismo_utm)
 seismo_utm_x_km = [x / 1000 for x in seismo_utm_x]
 seismo_utm_y_km = [y / 1000 for y in seismo_utm_y]
 
-file = open('C185data_atm_1o.txt', 'r')
+file = open('C185data_atm_full.txt', 'r')
 file2 = pd.read_csv('/home/irseppi/REPOSITORIES/parkshwynodal/input/all_station_crossing_db_C185.csv', sep=",")
 tail_nums = file2['TAIL_NUM']
 flight = file2['FLIGHT_NUM']
@@ -41,6 +40,9 @@ flight_lon = {}
 flight_alt = {}
 UTM_km_x = {}
 UTM_km_y = {}
+points_sta_lat = {}
+points_sta_lon = {}
+
 # Iterate over each line in the file
 for line in file.readlines():
     lines = line.split(',')
@@ -65,6 +67,8 @@ for line in file.readlines():
     for s in range(len(seismo_data)):
         if str(nodes) == str(stations[s]):
             seismometer = (seismo_utm_x_km[s], seismo_utm_y_km[s]) 
+            closet_station_lat = seismo_latitudes[s]
+            closet_station_lon = seismo_longitudes[s]
             break
         else:
             continue
@@ -116,6 +120,8 @@ for line in file.readlines():
         flight_alt[flight_num] = []
         UTM_km_x[flight_num] = []
         UTM_km_y[flight_num] = []
+        points_sta_lat[flight_num] = []
+        points_sta_lon[flight_num] = []
         flight_lat[flight_num].extend(flight_latitudes)
         flight_lon[flight_num].extend(flight_longitudes)
         UTM_km_x[flight_num].extend(flight_utm_x_km)
@@ -125,6 +131,8 @@ for line in file.readlines():
     all_med[flight_num].extend([np.nanmedian(f1)])
     points_lat[flight_num].extend([closest_lat])
     points_lon[flight_num].extend([closest_lon])
+    points_sta_lat[flight_num].extend([closet_station_lat])
+    points_sta_lon[flight_num].extend([closet_station_lon])
     flights.append(flight_num)
 
 flight_num2 = 0
@@ -142,7 +150,8 @@ for flight_num in flights:
     med = np.array(all_med[flight_num])
     fxx = np.array(UTM_km_x[flight_num])
     fyy = np.array(UTM_km_y[flight_num])
-    
+    p_sta_lat = np.array(points_sta_lat[flight_num])
+    p_sta_lon = np.array(points_sta_lon[flight_num])
     P_lat = np.array(points_lat[flight_num])
     P_lon = np.array(points_lon[flight_num])
 
@@ -177,13 +186,6 @@ for flight_num in flights:
                 elev_point.append(interpolated_alt)
                 break
 
-                # Avoid adding duplicate points
-                if len(dist_point) == 0 or not np.isclose(P_utm_x_km[j], P_utm_x_km[-1]):
-                    dist_point.append(projected_dist)
-                    
-                    # Interpolate the altitude at the point (P_utm_x_km[j], P_utm_y_km[j])
-                    interpolated_alt = alt_t[i] + projection_factor * (alt_t[i+1] - alt_t[i])
-                    elev_point.append(interpolated_alt)
             else:
                 continue
     print(dist_point, elev_point)
@@ -200,28 +202,33 @@ for flight_num in flights:
                 with pygmt.config(MAP_FRAME_TYPE='plain', FORMAT_GEO_MAP="ddd.xx"):
                     proj = "M15c"
                     fig.grdimage(grid=grid, projection=proj, frame="a", cmap="geo")
-                    fig.colorbar(frame=["a1000", "x+lElevation (m)"], position="JMR+o8c/6c+w11.5c/0.5c")
+                    fig.colorbar(frame=["a1000", "x+lElevation, m"], position="JMR+o8c/6c+w11.5c/0.5c")
                     fig.plot(x=np.array(f_lon), y=np.array(f_lat), pen="1p,black", projection=proj)
 
                     for i in range(len(f_lat) - 1):
-                        if i == 0 or i == len(f_lat) - 2:
-                            angle = np.arctan2(np.array(f_lat)[i + 1] - np.array(f_lat)[i], np.array(f_lon)[i + 1] - np.array(f_lon)[i])
+                        if i == 4:
+                            angle = np.arctan2(np.array(f_lat)[i + 4] - np.array(f_lat)[i], np.array(f_lon)[i + 4] - np.array(f_lon)[i])
                             angle = np.degrees(angle)
-                            no = np.sqrt((np.array(f_lon)[i + 1] - np.array(f_lon)[i]) ** 2 + (np.array(f_lat)[i + 1] - np.array(f_lat)[i]) ** 2)
-                            fig.plot(x=[np.array(f_lon)[i]], y=[np.array(f_lat)[i]], style="v0.1c+e", direction=[[angle], [0.2]], fill='black', pen="0.5p,black", projection=proj)
+                            fig.plot(x=[np.array(f_lon)[i]], y=[np.array(f_lat)[i]], style="v0.7c+e", direction=[[angle-22], [0.7]], fill='black', pen="1p,black", region=[-151.2, -150.05, 62.29, 63.15],projection=proj)
+                        elif i == len(f_lat) - 8:
+                            angle = np.arctan2(np.array(f_lat)[i + 2] - np.array(f_lat)[i], np.array(f_lon)[i + 2] - np.array(f_lon)[i])
+                            angle = np.degrees(angle)
 
+                            fig.plot(x=[np.array(f_lon)[i]], y=[np.array(f_lat)[i]], style="v0.7c+e", direction=[[angle-21], [1.5]], fill='black', pen="1p,black", region=[-151.2, -150.05, 62.29, 63.15],projection=proj)
+                        else:
+                            continue
                     fig.plot(x=seismo_longitudes, y=seismo_latitudes, style="x0.2c", pen="01p,black", projection=proj)
                     fig.plot(x=-150.1072713049972, y=62.30091781635389, style="x0.3c", pen="02p,pink", projection=proj)
                     pygmt.makecpt(cmap="gmt/seis", series=[np.min(med)-0.01, np.max(med)+0.01])
                     yy = fig.plot(x=lon, y=lat, style="c0.3c", fill=med, pen="black", cmap=True, projection=proj)
-                    fig.colorbar(frame=["a0.5f0.1", 'xaf+l\u0394'+'F (Hz)'], position="JMR+o8c/-6.5c+w11.5c/0.5c")
+                    fig.colorbar(frame=["a0.5f0.1", 'xaf+l\u0394'+'F, Hz'], position="JMR+o8c/-6.5c+w11.5c/0.5c")
 
                     zoom_region = [np.min(lon) - 0.01, np.max(lon) + 0.01, np.min(lat) - 0.01, np.max(lat) + 0.01]
                     rectangle = [[zoom_region[0], zoom_region[2], zoom_region[1], zoom_region[3]]]
                     fig.plot(data=rectangle, style="r+s", pen="0.5p,black", projection=proj)
 
             with fig.set_panel(panel=[0,0]):
-                zoom_region = [np.min(lon) - 0.01, np.max(lon) + 0.01, np.min(lat) - 0.01, np.max(lat)+ 0.01]
+                zoom_region = [np.min(lon) - 0.01, np.max(lon) + 0.01, np.min(lat) - 0.005, np.max(lat)+ 0.015]
                 with pygmt.config(MAP_FRAME_TYPE = 'plain',FORMAT_GEO_MAP="ddd.xx"):
                     grid_inset = pygmt.datasets.load_earth_relief(resolution="15s", region=zoom_region, registration="pixel")
                     cent_lon =  str(((np.max(lon) + 0.01) - np.min(lon) - 0.01)/2 + (np.min(lon) - 0.01))
@@ -233,8 +240,25 @@ for flight_num in flights:
                     fig.grdimage(grid=grid_inset, region=zoom_region, projection=proj,frame="a", cmap=True)
                     fig.plot(x=np.array(f_lon), y=np.array(f_lat), projection=proj, pen="1p,black") 
 
-                    fig.plot(x=seismo_longitudes, y=seismo_latitudes, projection=proj, style="x0.4c", pen="01p,black")
+                    for i in range(len(f_lat) - 1):
+                        if i == 26:
+                            angle = np.arctan2(np.array(f_lat)[i + 4] - np.array(f_lat)[i], np.array(f_lon)[i + 4] - np.array(f_lon)[i])
+                            angle = np.degrees(angle)
+                            fig.plot(x=[np.array(f_lon)[i]], y=[np.array(f_lat)[i]], style="v0.9c+e", direction=[[angle-21], [1]], fill='black', pen="1p,black", region=zoom_region,projection=proj)
+                        elif i == len(f_lat) - 10:
+                            angle = np.arctan2(np.array(f_lat)[i + 2] - np.array(f_lat)[i], np.array(f_lon)[i + 2] - np.array(f_lon)[i])
+                            angle = np.degrees(angle)
 
+                            fig.plot(x=[np.array(f_lon)[i]], y=[np.array(f_lat)[i]], style="v0.9c+e", direction=[[angle-10], [1]], fill='black', pen="1p,black", region=zoom_region,projection=proj)
+                        else:
+                            continue
+                    
+                    for tt in range(len(med)):
+                        x = [lon[tt], p_sta_lon[tt]]
+                        y = [lat[tt], p_sta_lat[tt]]
+                        fig.plot(x=x, y=y, pen="1p,black,-", connection="n", projection=proj)
+
+                    fig.plot(x=seismo_longitudes, y=seismo_latitudes, projection=proj, style="x0.6c", pen="2p,black")
                     pygmt.makecpt(cmap="gmt/seis", series=[np.min(med)-0.01, np.max(med)+0.01]) 
                     yy = fig.plot(x=lon, y=lat, style="c0.4c", fill=med, projection=proj, pen="black", cmap=True) 
 
@@ -305,7 +329,7 @@ for flight_num in flights:
             fig.basemap(
                 region=prof_region,  # x_min, x_max, y_min, y_max
                 projection=proj,
-                frame=["WSrt", "xa20+lDistance / km", "ya1000+lElevation / m"], 
+                frame=["WSrt", "xa20+lDistance, km", "ya1000+lElevation, m"], 
             )
 
             fig.plot(
@@ -340,7 +364,6 @@ for flight_num in flights:
                 region=prof_region,
             )
             pygmt.makecpt(cmap="gmt/seis", series=[np.min(med)-0.01, np.max(med)+0.01])
-            print(len(med),len(dist_point), len(elev_point))
             fig.plot(
                 x=np.array(dist_point),
                 y=np.array(elev_point),
@@ -354,4 +377,4 @@ for flight_num in flights:
     fig.savefig("output.png", dpi=300)
     fig.show(verbose="i") 
         
-    break
+    break #1st plot, 3rd plot or ...
